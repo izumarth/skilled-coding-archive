1464690c7b485b4dd03a268a7ae8d0434a8291f4
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-07-29 01:33:40 +0900

    add: basic get/post api

---
+++ 02_sqlc_etc/api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	db "izumarth_grpc/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+
+	return server
+}
+
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
