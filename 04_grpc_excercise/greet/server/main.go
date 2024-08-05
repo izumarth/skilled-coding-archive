d492298e12851500ce84b8de3fbeee9085d0718a
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-08-06 00:27:05 +0900

    add: 04 grpc example setup

---
+++ 04_grpc_excercise/greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/izumarth/skilled-coding-archive/04-grpc-excercise/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listin on: %v", err)
+	}
+
+	log.Printf("Listning on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
