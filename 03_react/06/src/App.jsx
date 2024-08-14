d38e732564ceb92f31fc824838417ec4e5086dc2
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-08-15 07:04:23 +0900

    add: react use effect clear

--- 03_react/06/src/App.jsx
+++ 03_react/06/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -16,7 +16,6 @@ function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
-
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -50,13 +49,13 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) => prevPickedPlaces.filter((place) => place.id !== selectedPlace.current));
     setModalOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(...[storedIds.filter((id) => id !== selectedPlace.current)]));
-  }
+  }, []);
 
   return (
     <>
