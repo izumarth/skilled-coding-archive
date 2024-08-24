f11cf80827cfb44f50b6ec6e153771dae04498f3
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-08-25 05:54:47 +0900

    add: react featch api start

---
+++ 03_react/09/src/components/AvailablePlaces.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsLoading(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
