a24b51c071f8d6f5fb59a9016e8fdd0207a9b9ea
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-08-29 05:18:06 +0900

    add: dp adapter pattern

---
+++ 05_go_design_pattern/client/cmd/web/setup_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"go-breeders/models"
+	"os"
+	"testing"
+)
+
+var testApp application
+
+func TestMain(m *testing.M) {
+	testBackend := &TestBackend{}
+	testAdapter := &RemoteService{Remote: testBackend}
+
+	testApp = application{
+		Models:     *models.New(nil),
+		catService: testAdapter,
+	}
+
+	os.Exit(m.Run())
+}
+
+type TestBackend struct{}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	breeds := []*models.CatBreed{
+		&models.CatBreed{ID: 1, Breed: "Tomcat", Details: "SomeDetails"},
+	}
+
+	return breeds, nil
+}
