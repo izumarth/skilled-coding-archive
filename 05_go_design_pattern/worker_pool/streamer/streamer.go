31f58f846fbac55f6f6366a14747b8e50d5ffe4a
tadaichiro nakano <tadnak@tadaichirononotobukkukonpyuta.local>
2024-09-04 08:37:23 +0900

    add: dp worker-pool start

---
+++ 05_go_design_pattern/worker_pool/streamer/streamer.go
@@ -0,0 +1,43 @@
+package streamer
+
+type ProcessingMessage struct {
+	ID         int
+	Successful bool
+	Message    string
+	OutputFile string
+}
+
+type VideoProcessingJob struct {
+	Video Video
+}
+
+type Processor struct {
+	Engine Encoder
+}
+
+type Video struct {
+	ID           int
+	InputFile    string
+	OutputDir    string
+	EncodingType string
+	NotifyChan   chan ProcessingMessage
+	// Options *VideoOption
+	Encoder Processor
+}
+
+func (v *Video) encode() {
+
+}
+
+func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
+	workerPool := make(chan chan VideoProcessingJob, maxWorkers)
+
+	p := Processor{}
+
+	return &VideoDispatcher{
+		jobQueue:   jobQueue,
+		maxWorkers: maxWorkers,
+		WorkerPool: workerPool,
+		Processor:  p,
+	}
+}
