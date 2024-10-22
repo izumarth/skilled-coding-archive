299e9cfe32c5b0ec1bf1fbade48b0951e6a87397
Izumarth <tadnak@tadaichirononotobukkukonpyuta.local>
2024-10-23 02:38:17 +0900

    add: react next js sqlite

--- 03_react/18_Nextjs_2/app/meals/page.js
+++ 03_react/18_Nextjs_2/app/meals/page.js
@@ -1,7 +1,25 @@
-export default function MelasPage() {
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+export default async function MelasPage() {
+  const meals = await getMeals();
+
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by your</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share hyour Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
